51d1a0ca513e4d7d5a82769b657a1d20af98aa8c
Pablo <52553480+elpablazo@users.noreply.github.com>
2023-01-30 19:30:03 -0600

    asdfasdf

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				base: '#84a59d',
+				light: '#ead4bc',
+				lighter: '#eae0d5',
+				primary: '#ee6c4d'
+			},
+			fontFamily: {
+				sans: 'Montserrat',
+				title: 'Aboreto'
+			}
+		}
 	},
 	plugins: []
 };
