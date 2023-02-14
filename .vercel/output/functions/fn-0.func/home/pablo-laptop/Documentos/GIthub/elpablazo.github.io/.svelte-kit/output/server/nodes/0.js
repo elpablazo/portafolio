fb3cca355ae2a6e47f5eb20d0a241a6703a2e584
Pablo <pablo.vazquezg@protonmail.com>
2023-02-13 19:08:04 -0600

    update de sveltekit

---
+++ .vercel/output/functions/fn-0.func/home/pablo-laptop/Documentos/GIthub/elpablazo.github.io/.svelte-kit/output/server/nodes/0.js
@@ -0,0 +1,8 @@
+
+
+export const index = 0;
+export const component = async () => (await import('../entries/pages/_layout.svelte.js')).default;
+export const file = '_app/immutable/components/pages/_layout.svelte-0b4cf987.js';
+export const imports = ["_app/immutable/components/pages/_layout.svelte-0b4cf987.js","_app/immutable/chunks/index-3b108106.js"];
+export const stylesheets = ["_app/immutable/assets/_layout-601d5352.css"];
+export const fonts = [];
---
+++ .vercel/output/static/_app/immutable/chunks/0-18f2d724.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-0b4cf987.js";export{t as component};
